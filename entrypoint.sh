ef9f29fb9b72267e7aa61a71964d7d259fff91b2
Oleg Fedoseev <o.fedoseev@office.ngs.ru>
2017-01-30 13:35:18 +0700

    Small fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ sed -i "s#{{TIMEZONE}}#${TIMEZONE}#" /etc/opentsdb/opentsdb.conf
 sed -i "s#{{COMPACTION}}#${COMPACTION}#" /etc/opentsdb/opentsdb.conf
 sed -i "s#{{FLUSH_INTERVAL}}#${FLUSH_INTERVAL}#" /etc/opentsdb/opentsdb.conf
 sed -i "s#{{ZK_QUORUM}}#${ZK_QUORUM}#" /etc/opentsdb/opentsdb.conf
-sed -i "s#{{ROOT_LOG_LEVEL}}#${ROOT_LOG_LEVEL}#"   -i /etc/opentsdb/logback.xml
-sed -i "s#{{QUERY_LOG_LEVEL}}#${QUERY_LOG_LEVEL}#" -i /etc/opentsdb/logback.xml
+sed -i "s#{{ROOT_LOG_LEVEL}}#${ROOT_LOG_LEVEL}#" /etc/opentsdb/logback.xml
+sed -i "s#{{QUERY_LOG_LEVEL}}#${QUERY_LOG_LEVEL}#" /etc/opentsdb/logback.xml
 
 exec /usr/bin/tsdb "$@"
